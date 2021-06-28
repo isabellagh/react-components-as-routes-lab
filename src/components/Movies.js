48789eab3d1f896451121f439d5c5c58040aabf6
isabellagh <isabellagh88@gmail.com>
2021-06-28 17:20:07 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,21 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h3>Movie: {movie.title}</h3>
+          <ul>
+            <li>Time: {movie.time}</li>
+            <li>Genre: {movie.genres}</li>
+            <ul>
+              {movie.genres.map((genre, index) => (
+                <li key={index}>{genre}</li>
+              ))}
+            </ul>
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
